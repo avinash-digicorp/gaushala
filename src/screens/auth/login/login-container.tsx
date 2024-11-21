162488a5670ee4c7b859179e1807cfa6c5dcf14d
avinash <avinashrathod506@gmail.com>
2024-11-21 15:53:34 +0530

    google sign in ios

--- src/screens/auth/login/login-container.tsx
+++ src/screens/auth/login/login-container.tsx
@@ -1,59 +1,61 @@
-import {useEffect, useState} from 'react'
-import {GoogleSignin} from '@react-native-google-signin/google-signin'
-import {useDispatch} from 'react-redux'
-import {login} from 'store/auth/slice'
-import {useToast} from 'components'
-import {hasTextLength} from 'utils'
-import {navigateTo, routes} from 'navigation'
+import {useEffect, useState} from 'react';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import {useDispatch} from 'react-redux';
+import {login} from 'store/auth/slice';
+import {useToast} from 'components';
+import {hasTextLength} from 'utils';
+import {navigateTo, routes} from 'navigation';
 
 const useLoginContainer = () => {
-  const dispatch = useDispatch()
-  const {showToast} = useToast()
-  const [mobile, setMobile] = useState<string>('')
+  const dispatch = useDispatch();
+  const {showToast} = useToast();
+  const [mobile, setMobile] = useState<string>('');
   useEffect(() => {
-    GoogleSignin.configure({})
-  }, [])
+    GoogleSignin.configure({});
+  }, []);
 
   const loginSuccess = () => {
     showToast({
       title: 'Login Success',
-      autodismiss: true
-    })
-  }
+      autodismiss: true,
+    });
+  };
 
   const googleSignIn = async () => {
     try {
-      await GoogleSignin.signOut()
-      await GoogleSignin.hasPlayServices()
-      const userInfo = await GoogleSignin.signIn()
-      const data = await dispatch(login(userInfo.user))
-      await loginSuccess()
-    } catch (error) {}
-  }
+      await GoogleSignin.signOut();
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      const data = await dispatch(login(userInfo.user));
+      await loginSuccess();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const onSubmit = async () => {
     try {
       if (!hasTextLength(mobile)) {
         showToast({
           title: 'Mobile is required',
-          autodismiss: true
-        })
-        return
+          autodismiss: true,
+        });
+        return;
       }
       const onSuccess = async () => {
-        await dispatch(login({mobile}))
-        loginSuccess()
-      }
-      navigateTo(routes.VERIFY_OTP, {mobile, onSuccess})
+        await dispatch(login({mobile}));
+        loginSuccess();
+      };
+      navigateTo(routes.VERIFY_OTP, {mobile, onSuccess});
     } catch (error) {}
-  }
+  };
 
-  const states = {mobile, setMobile}
-  const handlers = {googleSignIn, onSubmit}
+  const states = {mobile, setMobile};
+  const handlers = {googleSignIn, onSubmit};
   return {
     ...states,
-    ...handlers
-  }
-}
+    ...handlers,
+  };
+};
 
-export default useLoginContainer
+export default useLoginContainer;
